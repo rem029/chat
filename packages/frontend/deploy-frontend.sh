fd79a3732040d2486431570fd3ceb93ca9a3d6b2
Rem Ponce <elawrenceponce@gmail.com>
2023-04-22 12:53:00 +0300

    fix(DEPLOY): github action update

--- packages/frontend/deploy-frontend.sh
+++ packages/frontend/deploy-frontend.sh
@@ -1,15 +1,15 @@
-# echo "***Creating nginx files if not existing"
-# sudo cp -n ./nginx/chat /etc/nginx/sites-available/chat
+echo "***Creating nginx files if not existing"
+sudo cp -n ./nginx/chat /etc/nginx/sites-available/chat
 
-# echo "***Linking nginx files"
-# cd /etc/nginx/sites-enabled/
-# sudo ln -s /etc/nginx/sites-available/chat
+echo "***Linking nginx files"
+cd /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/chat
 
-# echo "***Verify and restart nginx"
-# sudo nginx -t
-# nginx -s reload
-# systemctl restart nginx
-# sudo service nginx restart
+echo "***Verify and restart nginx"
+sudo nginx -t
+nginx -s reload
+systemctl restart nginx
+sudo service nginx restart
 
-# echo "***Run certbot"
-# certbot --nginx -d chat.rem029.com --redirect --agree-tos --non-interactive
+echo "***Run certbot"
+certbot --nginx -d chat.rem029.com --redirect --agree-tos --non-interactive
